776d479ae1b3a05821f9e151a6a00cf3c8d415f4
Dion Garman <diongarman@gmail.com>
2020-06-03 15:11:51 +0000

    implemented insert() for LL

--- index.js
+++ index.js
@@ -12,48 +12,90 @@
 //   }
 // };
 
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null
-    };
+    this.head = new Node(value)
     this.tail = this.head;
     this.length = 1;
   }
 
   prepend(value) {
 
-    let head = {
-      value: value,
-      next: this.head
-    }
+    let newNode = new Node(value)
+    newNode.next = this.head
 
-    this.head = head
+    //update object state
+    this.head = newNode
     this.length++
      
+     return this
 
 
   }
   append(value) {
     //Code here
-    const newNode = {
-      value: value,
-      next: null
-    }
+    const newNode = new Node(value)
 
     this.tail.next = newNode; 
 
     //update object state
     this.tail = newNode
     this.length++;
+
+    return this
+  }
+
+
+  insert(i, value) {
+    const newNode = new Node(value)
+
+
+    if (i <= 1) {
+      this.prepend(value)
+      return this
+    }
+
+
+    let prev;
+    let curr = this.head
+   
+    while(i-1>=1) {
+      prev = curr
+      curr = curr.next
+      i--
+    }
+    prev.next = newNode
+    prev.next.next = curr
+
+    this.length++
+    // console.log(prev)
+    // console.log(curr)
+
+    return this
   }
 }
 
+
+// 1-->2-->3
+// 7-->8-->69-->null
+//insert(2,17)
+// 7-->8-->17
+
+
+
+//8-10-5-16-19
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-myLinkedList.append(17);
-myLinkedList.prepend(7)
-
-console.log(myLinkedList)
+myLinkedList.append(19);
+myLinkedList.prepend(8)
+myLinkedList.insert(2,17)
+//8-17-10-5-16-19
