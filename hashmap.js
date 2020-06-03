e8ce9a3a4bf5fd3cdfc763d658210fda486336d7
Dion Garman <diongarman@gmail.com>
2020-06-03 13:39:16 +0000

    Implemented 'append' method to Linked List Class

---
+++ hashmap.js
@@ -0,0 +1,80 @@
+class HashTable {
+  constructor(size){
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i =0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+  }
+  get(k){
+
+    let address = this._hash(k)
+    let currBucket = this.data[address]
+    //console.log(currBucket)
+    if(currBucket) {
+      for(let i = 0; i < currBucket.length; i++ ){
+        if(currBucket[i][0] === k) {
+          return currBucket[i][1]
+        }
+      }
+    }
+    return undefined
+  }
+
+  keys(){
+     if (!this.data.length) {
+      return undefined
+    }
+    const keysArray = []
+    for(let i = 0; i < this.data.length; i++){
+      
+      if(this.data[i]) {
+
+        if (this.data[i].length > 1) {
+
+          for(let j = 0; this.data[i].length;j++){
+            keysArray.push(this.data[i][j][0])
+          }
+
+        }
+        
+
+      //no collisions
+      else  {
+        
+        keysArray.push(this.data[i][0][0])
+      }
+
+      } 
+    }
+
+    return keysArray
+  }
+
+  set(k, v) {
+
+    let address = this._hash(k)
+
+    if(!this.data[address]){
+      this.data[address] = []
+    }
+    this.data[address].push([k,v])
+    return this.data
+  }
+
+
+
+
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('apples', 99)
+myHashTable.get('apples')
+myHashTable.set('grapes', 10000)
+myHashTable.get('grapes')
+console.log(myHashTable.keys())
+
--- index.js
+++ index.js
@@ -1,80 +1,47 @@
-class HashTable {
-  constructor(size){
-    this.data = new Array(size);
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
   }
-
-  _hash(key) {
-    let hash = 0;
-    for (let i =0; i < key.length; i++){
-        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+  append(value) {
+    //Code here
+    let ref = this.head;
+    
+    while (ref.next) {
+      ref = ref.next
     }
-    return hash;
-  }
-  get(k){
-
-    let address = this._hash(k)
-    let currBucket = this.data[address]
-    //console.log(currBucket)
-    if(currBucket) {
-      for(let i = 0; i < currBucket.length; i++ ){
-        if(currBucket[i][0] === k) {
-          return currBucket[i][1]
-        }
-      }
-    }
-    return undefined
-  }
-
-  keys(){
-     if (!this.data.length) {
-      return undefined
-    }
-    const keysArray = []
-    for(let i = 0; i < this.data.length; i++){
-      
-      if(this.data[i]) {
-
-        if (this.data[i].length > 1) {
-
-          for(let j = 0; this.data[i].length;j++){
-            keysArray.push(this.data[i][j][0])
-          }
-
-        }
-        
-
-      //no collisions
-      else  {
-        
-        keysArray.push(this.data[i][0][0])
-      }
-
-      } 
+    
+    ref.next = {
+      value: value,
+      next: null
     }
 
-    return keysArray
+    this.tail = ref.next  
+    this.length++
   }
-
-  set(k, v) {
-
-    let address = this._hash(k)
-
-    if(!this.data[address]){
-      this.data[address] = []
-    }
-    this.data[address].push([k,v])
-    return this.data
-  }
-
-
-
-
 }
 
-const myHashTable = new HashTable(50);
-myHashTable.set('apples', 99)
-myHashTable.get('apples')
-myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
-console.log(myHashTable.keys())
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(17);
 
+console.log(myLinkedList)
