4b468c5dc43ffd7c096bafe809d1a7c5d32d7839
Dion Garman <diongarman@gmail.com>
2020-06-30 14:10:51 +0000

    Andrei's more readable selection sort implementation

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 function selectionSort(array) {
 
   let {length} = array
-  let minimum;
-  let temp;
-  let swapIndex
+
 
   for(let i = 0; i < length; i++ ) {
+
+      let minimum = i;
+      let temp = array[i];
+      
       for(let j = i+1; j < length; j++ ) {
         if (array[j] < array[i]) {
-          minimum = array[j]
-          swapIndex = j
+   
+          minimum = j
      
         }
     }
 
-    if (minimum !== null) {
-      temp = array[i];
-      array[i] = minimum;
-      array[swapIndex] = temp
-      minimum = null
-    }
+    array[i] = array[minimum];
+    array[minimum] = temp
     
   }
 
