d25cc6e4a17cd9280c59928a1a4d7e7fcafee142
Dion Garman <diongarman@gmail.com>
2020-07-02 10:56:24 +0000

    updated insertion sort to use same nested while loop as mycodeschool

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
+
+//While loop logic stolen from mycodeschool
 function insertionSort(array) {
   let {length} = array
   //init boundary
-  let boundary = 0;
+  let hole
 
   let value;
 
   for (let i = 1; i < length; i++) {
     value = array[i]
+    hole = i
 
-    for (let j = i-1; j >= 0; j--) {
-      let temp;
-
-      if (array[j] > value) {
-        temp = array[j]
-        array[j+1] = temp
-        array[j] = value
-      } 
+    while(hole > 0 && array[hole - 1] > value) {
+      array[hole] = array[hole - 1];
+      hole--
     }
+    array[hole] = value
   }
   
 }
+
+
+// function insertionSort(array) {
+//   let {length} = array
+//   //init boundary
+//   let boundary = 0;
+
+//   let value;
+
+//   for (let i = 1; i < length; i++) {
+//     value = array[i]
+
+//     for (let j = i-1; j >= 0; j--) {
+//       let temp;
+
+//       if (array[j] > value) {
+//         temp = array[j]
+//         array[j+1] = temp
+//         array[j] = value
+//       } 
+//     }
+//   }
+  
+// }
 
 insertionSort(numbers);
 console.log(numbers);
